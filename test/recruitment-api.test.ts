db18fd0bd5fa8ee55ccc408d319d67999ea8c354
Toseef Ahmed <tauseefahmed92b@gmail.com>
2024-08-24 21:29:01 +0300

    Initial commit

---
+++ test/recruitment-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecruitmentApi from '../lib/recruitment-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recruitment-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecruitmentApi.RecruitmentApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
